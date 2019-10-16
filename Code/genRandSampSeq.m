a3bd7f04a9ea929afbf9154037a6428336c5f715
sliang11 <46596561+sliang11@users.noreply.github.com>
2019-10-16 18:37:48 +0800

    Add files via upload

---
+++ Code/genRandSampSeq.m
@@ -0,0 +1,40 @@
+function genRandSampSeq(dataset, datasetPath, ST1NNSeedPath, modelSelectSeedPath)
+
+numRandRuns = 10;
+initNumPLabeled = 1;
+pLabel = 1;
+
+data = load(fullfile(datasetPath, dataset, [dataset, '_TRAIN.tsv']));
+trainLabels = data(:, 1);
+numTrain = length(trainLabels);
+numP = length(find(trainLabels == pLabel));
+
+numSeeds = min(numP, 10);
+seeds = load(fullfile(ST1NNSeedPath, ['seeds_', dataset, '_', num2str(numSeeds), '_1.txt']));
+
+numU = numTrain - initNumPLabeled;
+for j = 1 : numSeeds
+    seed = seeds(j);
+    unlabeledInds = setdiff(1 : numTrain, seed);
+    
+    randSeqs = zeros(numRandRuns, numU);
+    for k = 1 : numRandRuns
+        seq = unlabeledInds(randperm(numU));
+        while ismember(seq, randSeqs, 'rows')
+            seq = unlabeledInds(randperm(numU));
+        end
+        randSeqs(k, :) = seq;
+    end
+    
+    fName = fullfile(modelSelectSeedPath, [dataset, '_randSampSequences_rand10_single_', ...
+        num2str(numRandRuns), '_', num2str(j - 1), '.txt']);
+    fid = fopen(fName, 'w');
+    for k = 1 : numRandRuns
+        seq = randSeqs(k, :);
+        for t = 1 : numU
+            fprintf(fid, '%d ', seq(t));
+        end
+        fprintf(fid, '\n');
+    end
+    fclose(fid);
+end
---
+++ Code/genST1NNSeeds.m
@@ -0,0 +1,32 @@
+function genST1NNSeeds(dataset, ST1NNSeedPath, datasetPath)
+
+pLabel = 1;
+numLabeled = 1;
+
+%load the dataset
+data = load(fullfile(datasetPath, dataset, [dataset, '_TRAIN.tsv']));
+
+%labels
+labels = data(:, 1);
+pInds = (find(labels == pLabel))';
+numP = length(pInds);
+numSeeds = min(numP, 10);
+
+seeds = zeros(numSeeds, numLabeled);
+for i = 1 : numSeeds
+    seed = pInds(sort(random('unid', numP, 1, numLabeled)));
+	while(ismember(seed, seeds, 'rows') || length(unique(seed)) < numLabeled)
+		seed = pInds(sort(random('unid', numP, 1, numLabeled)));
+	end
+	seeds(i, :) = seed;
+end
+
+fName = fullfile(ST1NNSeedPath, ['seeds_', dataset, '_', num2str(numSeeds), '_', num2str(numLabeled) '.txt']);
+fid = fopen(fName, 'w');
+for i = 1 : numSeeds
+    for j = 1 : numLabeled
+        fprintf(fid, '%d ', seeds(i, j));
+    end
+    fprintf(fid, '\n');
+end
+fclose(fid);
